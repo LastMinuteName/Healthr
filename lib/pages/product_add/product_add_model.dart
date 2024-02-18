5f6a0a8e184fb4620f19ca1ac96f6b98f6b61b96
LastMinuteName <LastMinuteName@hotmail.com>
2024-02-19 00:30:07 +1100

    Revert "added changenotifierprovider to product_add page for state management"
    This reverts commit 82dac1348a640388f1d31c96a3d059d5cdba336d.

--- lib/pages/product_add/product_add_model.dart
+++ lib/pages/product_add/product_add_model.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class ProductAddModel with ChangeNotifier {
-  List<String> categoryList = [
-    "Food & Drinks",
-    "Cosmetic"
-  ];
-
   int productAddStageIndex = 0;
 
-  int category = 0;
+  String category = "Food & Drinks";
 
   var brandNameController = TextEditingController();
   var productNameController = TextEditingController();
   bool organicValue = false;
-
-  void nextStage() {
-    productAddStageIndex++;
-  }
-
-  void prevStage() {
-    if (productAddStageIndex > 0) {
-      productAddStageIndex--;
-    }
-  }
 }
