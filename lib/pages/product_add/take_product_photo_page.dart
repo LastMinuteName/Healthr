d50912ec48770b08cc2d37c9c3d7a55e21a9e574
LastMinuteName <LastMinuteName@hotmail.com>
2024-03-17 14:52:45 +1100

    Added overlay to the page as well as a dialogue box to test photo function

--- lib/pages/product_add/take_product_photo_page.dart
+++ lib/pages/product_add/take_product_photo_page.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../../widgets/camera_capture_icon_button.dart';
 
 class TakeProductPhotoPage extends StatefulWidget {
   @override
@@ -18,6 +23,19 @@ class _TakeProductPhotoPageState extends State<TakeProductPhotoPage> {
       _initializeCamera();
       _showAlertDialog(context);
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      resizeToAvoidBottomInset: false,
+      child: Stack(
+        children: [
+          cameraView(),
+          cameraOverlay(),
+        ],
+      ),
+    );
   }
 
   Future<void> _initializeCamera() async {
@@ -65,12 +83,52 @@ class _TakeProductPhotoPageState extends State<TakeProductPhotoPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
+  Widget cameraOverlay() {
+    return SafeArea(
       child: Stack(
         children: [
-          cameraView(),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.flashlight_off),
+              iconSize: 32.0,
+              onPressed: () {  },
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Text(
+              AppLocalizations.of(context)!.scanHeader,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              CameraCaptureIconButton(
+                size: MediaQuery.of(context).size.width * 0.2,
+                callback: () async {
+                  XFile? picture = await controller?.takePicture();
+
+                  print(picture!.path);
+
+                  //Directory directory = await getApplicationDocumentsDirectory();
+
+                  showCupertinoModalPopup<void>(
+                    context: context,
+                    builder: (BuildContext context) => CupertinoAlertDialog(
+                      content: Image.file(File(picture!.path)),
+                    ),
+                  );
+                }
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
         ],
       ),
     );
@@ -79,6 +137,8 @@ class _TakeProductPhotoPageState extends State<TakeProductPhotoPage> {
   Widget cameraView() {
     if (controller != null) {
       if (controller!.value.isInitialized) {
+        //Scaling implementation is from https://stackoverflow.com/questions/56735552/how-to-set-flutter-camerapreview-size-fullscreen
+        //TODO: Understand how the CustomClipper class works before implementing
         final scale = 1 / (controller!.value.aspectRatio * MediaQuery.of(context).size.aspectRatio);
         return Transform.scale(
           scale: scale,
